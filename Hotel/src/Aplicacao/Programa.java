9ad840549e21cbabb2c92a2405f5dc7227d8584e
cathio-dev <ramalho.marcos7@gmail.com>
2025-05-26 13:31:17 -0300

    First Commit - my branch

--- Hotel/src/Aplicacao/Programa.java
+++ Hotel/src/Aplicacao/Programa.java
@@ -3,5 +3,6 @@ package Aplicacao;
 public class Programa {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        System.out.println("TESTE BRANCH MARCOS RAMALHO");
     }
 }
