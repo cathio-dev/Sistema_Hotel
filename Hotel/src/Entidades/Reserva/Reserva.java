37375febf76ab2bd7e74708208a5265e400cb546
Suianne Martins <martinssuianne@gmail.com>
2025-06-05 21:57:52 -0300

    modificando reserva para conter apenas um quarto e um hospede

--- Hotel/src/Entidades/Reserva/Reserva.java
+++ Hotel/src/Entidades/Reserva/Reserva.java
@@ -1,74 +1,83 @@
-
 import java.time.LocalDate;
 
-public class Reserva{
+public class Reserva {
     private LocalDate entrada;
     private LocalDate saida;
-    protected Double valorTotal;
-    private GerenciadorQuartos gerenciadorQuartos;
-    private GerenciadorServicos gerenciadorServicos;
+    private Double valorTotal;
+    private Quarto quarto;
+    private Hospede hospede;
+    private GerenciadorDeServicos gerenciadorServicos;
 
-    public Reserva(){
+    public Reserva() {
 
+        this.gerenciadorServicos = new GerenciadorDeServicos();
     }
 
     public Reserva(LocalDate entrada, LocalDate saida, Double valorTotal) {
         this.entrada = entrada;
         this.saida = saida;
         this.valorTotal = valorTotal;
-        this.gerenciadorQuartos = new GerenciadorDeQuartos();
         this.gerenciadorServicos = new GerenciadorDeServicos();
     }
 
-	public LocalDate getEntrada() {
-		return entrada;
-	}
+    // Getters e Setters
+    public LocalDate getEntrada() {
+        return entrada;
+    }
 
-	public void setEntrada(LocalDate entrada) {
-		this.entrada = entrada;
-	}
+    public void setEntrada(LocalDate entrada) {
+        this.entrada = entrada;
+    }
 
-	public LocalDate getSaida() {
-		return saida;
-	}
+    public LocalDate getSaida() {
+        return saida;
+    }
 
-	public void setSaida(LocalDate saida) {
-		this.saida = saida;
-	}
+    public void setSaida(LocalDate saida) {
+        this.saida = saida;
+    }
 
-	public Double getValorTotal() {
-		return valorTotal;
-	}
+    public Double getValorTotal() {
+        return valorTotal;
+    }
 
-	public void setValorTotal(Double valorTotal) {
-		this.valorTotal = valorTotal;
-	}
+    public void setValorTotal(Double valorTotal) {
+        this.valorTotal = valorTotal;
+    }
 
-	public GerenciadorDeQuartos getGerenciadorQuartos() {
-		return gerenciadorQuartos;
-	}
+    public Quarto getQuarto() {
+        return quarto;
+    }
 
-	public void setGerenciadorQuartos(GerenciadorDeQuartos gerenciadorQuartos) {
-		this.gerenciadorQuartos = gerenciadorQuartos;
-	}
+    public void setQuarto(Quarto quarto) {
+        this.quarto = quarto;
+    }
 
-	public GerenciadorDeServicos getGerenciadorServicos() {
-		return gerenciadorServicos;
-	}
+    public Hospede getHospede() {
+        return hospede;
+    }
 
-	public void setGerenciadorServicos(GerenciadorDeServicos gerenciadorServicos) {
-		this.gerenciadorServicos = gerenciadorServicos;
-	}
+    public void setHospede(Hospede hospede) {
+        this.hospede = hospede;
+    }
+
+    public GerenciadorDeServicos getGerenciadorServicos() {
+        return gerenciadorServicos;
+    }
+
+    public void setGerenciadorServicos(GerenciadorDeServicos gerenciadorServicos) {
+        this.gerenciadorServicos = gerenciadorServicos;
+    }
 
     @Override
-public String toString() {
-    return "Reserva {" +
-            "entrada=" + entrada +
-            ", saida=" + saida +
-            ", valorTotal=" + valorTotal +
-            ", gerenciadorQuartos=" + (gerenciadorQuartos != null ? gerenciadorQuartos.toString() : "null") +
-            ", gerenciadorServicos=" + (gerenciadorServicos != null ? gerenciadorServicos.toString() : "null") +
-            '}';
-}
-
+    public String toString() {
+        return "Reserva {" +
+                "entrada=" + entrada +
+                ", saida=" + saida +
+                ", valorTotal=" + valorTotal +
+                ", quarto=" + (quarto != null ? quarto.toString() : "null") +
+                ", hospede=" + (hospede != null ? hospede.getNome() : "null") +
+                ", gerenciadorServicos=" + (gerenciadorServicos != null ? gerenciadorServicos.toString() : "null") +
+                '}';
+    }
 }
