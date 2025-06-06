627546c368a49aa5006499e3e3f5cf87257115aa
Suianne Martins <martinssuianne@gmail.com>
2025-06-05 21:49:32 -0300

    Adicionando classe HospedeCorporativo implementando classe Hospede

--- Hotel/src/Entidades/Hospede/HospedeCorporativo.java
+++ Hotel/src/Entidades/Hospede/HospedeCorporativo.java
@@ -1,10 +1,31 @@
 package Hospede
 
-public class HospedeCorporativo extends Hospede{
+public class HospedeCorporativo implements Hospede{
     
+    private String nome;
+    private Integer cadastro;
+
+    //Desconto fixo para hospede corporativo
+    public static final double DESCONTO_HOSPEDECORPORATIVO = 0.10;
+
     @Override
-    public Double calcularDesconto(){
-        return reserva.getValorTotal() * 0.10;
+    public String getNome(){
+        return nome;
+    }
+
+    @Override
+    public void setNome(String nome) { 
+        this.nome = nome; 
+    }
+
+    @Override
+    public Integer getCadastro() { 
+        return cadastro; 
+    }
+
+    @Override
+    public void setCadastro(Integer cadastro) {
+        this.cadastro = cadastro; 
     }
 
 }
